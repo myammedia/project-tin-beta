95e905358ef56b60f0b84561f1f5fcca44881357
myammedia <77873800+myammedia@users.noreply.github.com>
2021-03-02 11:59:52 +0530

    created about page

--- tiys/pages/views.py
+++ tiys/pages/views.py
@@ -5,3 +5,12 @@ def index(request):
     template = 'pages/home.html'
 
     return render(request, template)
+
+
+def about(request):
+    # category_list = Channel.objects.order_by('name')
+    # subscriber_list = Subscriber.objects.all()
+    # context = {'category_list': category_list, 'subscriber_list': subscriber_list}
+    template = 'pages/about.html'
+
+    return render(request, template)
