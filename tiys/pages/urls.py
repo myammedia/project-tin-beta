7193e24166a005d19c22487345f743ee93517631
myammedia <77873800+myammedia@users.noreply.github.com>
2021-03-02 12:22:43 +0530

    created contact page

--- tiys/pages/urls.py
+++ tiys/pages/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name="homepage"),
     path('about-us', views.about, name="about-page"),
+    path('contact-us', views.contact, name="contact-page"),
 ]
--- tiys/pages/views.py
+++ tiys/pages/views.py
@@ -14,3 +14,10 @@ def about(request):
     template = 'pages/about.html'
 
     return render(request, template)
+
+
+def contact(request):
+
+    template = 'pages/contact.html'
+
+    return render(request, template)
