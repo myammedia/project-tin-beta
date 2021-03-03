b8d42d39b1ba3cea3fb9a401ab6e7f36385c76e9
myammedia <77873800+myammedia@users.noreply.github.com>
2021-03-03 12:50:33 +0530

    created youtuber app

---
+++ tiys/youtuber/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+from category.models import Channel, Subscriber
+
+
+class Profile(models.Model):
+    channel_name = models.CharField(max_length=150, unique=True)
+    slug = models.SlugField(max_length=100, null=True,  unique=True)
+    channel_owner = models.CharField(max_length=100, blank=True)
+    image = models.ImageField(blank=True, upload_to='images/youtuber')
+    channel_category = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    subscriber_category = models.ForeignKey(Subscriber, on_delete=models.CASCADE)
+    join_date = models.DateField()
+    first_upload = models.DateField()
+    youtube_url = models.URLField(max_length=200)
+    instagram_url = models.URLField(max_length=200, blank=True)
+    twitter_url = models.URLField(max_length=200, blank=True)
+    facebook_url = models.URLField(max_length=200, blank=True)
+    website_url = models.URLField(max_length=200, blank=True)
+
+    def __str__(self):
+        return self.channel_name
+
+
+class Subsdata(models.Model):
+    profile_subsdata = models.ForeignKey(Profile, on_delete=models.CASCADE)
+    total_subs = models.CharField(max_length=20)
+    total_videos = models.IntegerField()
+    total_views = models.IntegerField()
+    update_date = models.DateField(auto_now_add=True, blank=True)
+
