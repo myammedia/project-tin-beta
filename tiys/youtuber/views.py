690714bd63d68ae1700c9bc373e20c399ca96215
myammedia <77873800+myammedia@users.noreply.github.com>
2021-03-03 13:09:12 +0530

    added youtuber index view

--- tiys/youtuber/views.py
+++ tiys/youtuber/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from category.models import Channel, Subscriber
 
-# Create your views here.
+
+def index(request):
+    youtuber_category_list = Channel.objects.order_by('name')
+    context = {'youtuber_category_list': youtuber_category_list}
+    template = 'youtuber/youtuber-index.html'
+
+    return render(request, template, context)
+
