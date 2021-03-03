4a8cfe175bbfe4277a43ff01d4cf579a7cb1ac08
myammedia <77873800+myammedia@users.noreply.github.com>
2021-03-03 13:18:13 +0530

    added youtuber list view

--- tiys/youtuber/views.py
+++ tiys/youtuber/views.py
@@ -17,3 +17,13 @@ def subscat_list_view(request, slug):
 
     return render(request, template, context)
 
+
+def youtuber_list_view(request, channel_category, subscriber_category):
+    channel_category = Channel.objects.get(slug=channel_category)
+    subscriber_category = Subscriber.objects.get(slug=subscriber_category)
+    youtuber_list = Profile.objects.filter(channel_category=channel_category, subscriber_category=subscriber_category)
+    context = {'channel_category': channel_category, 'subscriber_category': subscriber_category,
+               'youtuber_list': youtuber_list}
+    template = 'youtuber/youtuber-list.html'
+
+    return render(request, template, context)
