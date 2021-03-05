d0d378e6b2e08e33f5660d3f7a20152589f051ee
myammedia <admin@myam.media>
2021-03-05 13:02:27 +0530

    added channel profile view

--- tiys/channel/urls.py
+++ tiys/channel/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('<slug:slug>/', views.subscat_list_view, name="channel-subscat-list"),
     path('<slug:channel_category>/<slug:subscriber_category>/', views.channel_list_view,
          name="channel-list"),
+    path('<slug:channel_category>/<slug:subscriber_category>/<slug:slug>',
+         views.channel_profile_view, name="channel-profile")
 ]
