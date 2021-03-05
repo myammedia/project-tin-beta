07331366ed1b0c9eba0aef446ca7042e97b15f5c
myammedia <admin@myam.media>
2021-03-05 12:59:32 +0530

    added channel list url

--- tiys/channel/urls.py
+++ tiys/channel/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="channel-index"),
     path('<slug:slug>/', views.subscat_list_view, name="channel-subscat-list"),
+    path('<slug:channel_category>/<slug:subscriber_category>/', views.channel_list_view,
+         name="channel-list"),
 ]
