fa6b4ebe8e9604dea9a323ba2d59125b7929f380
myammedia <admin@myam.media>
2021-03-05 11:03:43 +0530

    created index view

--- tiys/channel/views.py
+++ tiys/channel/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from category.models import Channel, Subscriber
 
-# Create your views here.
+
+def index(request):
+    channel_category_list = Channel.objects.order_by('name')
+    context = {'channel_category_list': channel_category_list}
+    template = 'channel/channel-index.html'
+
+    return render(request, template, context)
