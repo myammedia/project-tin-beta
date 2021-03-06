19f6c2f852825b57af469a5d074511e152dde447
myammedia <admin@myam.media>
2021-03-06 12:35:39 +0530

    added youtuber subscat url

--- tiys/top_five/urls.py
+++ tiys/top_five/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name="top-five-index"),
     path('youtuber/', views.youtuber_category_view, name="top-five-youtuber-category"),
+    path('youtuber/<slug:slug>/', views.youtuber_subscat_view, name="top-five-youtuber-subscat"),
 
 ]
