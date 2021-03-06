8536fc87b0eae9a28c9ded20c806bda77ff5db9f
myammedia <admin@myam.media>
2021-03-06 12:26:21 +0530

    added youtuber category view

--- tiys/top_five/views.py
+++ tiys/top_five/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from category.models import Channel, Subscriber
 
 
 def index(request):
     template = 'top_five/top-five-index.html'
 
     return render(request, template)
+
+
+def youtuber_category_view(request):
+    youtuber_category_list = Channel.objects.order_by('name')
+    context = {'youtuber_category_list': youtuber_category_list}
+    template = 'top_five/top-five-youtuber-category.html'
+
+    return render(request, template, context)
