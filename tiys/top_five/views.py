d35e0ae963a8f9216899d514a04c8469ab471351
myammedia <admin@myam.media>
2021-03-06 12:29:28 +0530

    added youtuber subscat view

--- tiys/top_five/views.py
+++ tiys/top_five/views.py
@@ -14,3 +14,11 @@ def youtuber_category_view(request):
     template = 'top_five/top-five-youtuber-category.html'
 
     return render(request, template, context)
+
+
+def youtuber_subscat_view(request, slug):
+    channel_category = get_object_or_404(Channel, slug=slug)
+    context = {'channel_category': channel_category}
+    template = "top_five/top-five-youtuber-subscat.html"
+
+    return render(request, template, context)
