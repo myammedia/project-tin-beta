75545e5fa830fbf1998782188eb55c5d9e2cb252
myammedia <admin@myam.media>
2021-03-06 13:58:48 +0530

    added youtuber archive url

--- tiys/top_five/urls.py
+++ tiys/top_five/urls.py
@@ -8,5 +8,7 @@ urlpatterns = [
     path('youtuber/<slug:slug>/', views.youtuber_subscat_view, name="top-five-youtuber-subscat"),
     path('youtuber/<slug:channel_category>/<slug:subscriber_category>/', views.top_five_youtuber_view,
          name="top-five-youtuber-list"),
+    path('youtuber/<slug:channel_category>/<slug:subscriber_category>/archives', views.top_five_youtuber_view,
+         name="top-five-youtuber-archives"),
 
 ]
