2116af76d98b266a78cebdfc02e25b7891734838
myammedia <admin@myam.media>
2021-03-06 13:57:11 +0530

    added top five youtuber list url

--- tiys/top_five/urls.py
+++ tiys/top_five/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('', views.index, name="top-five-index"),
     path('youtuber/', views.youtuber_category_view, name="top-five-youtuber-category"),
     path('youtuber/<slug:slug>/', views.youtuber_subscat_view, name="top-five-youtuber-subscat"),
+    path('youtuber/<slug:channel_category>/<slug:subscriber_category>/', views.top_five_youtuber_view,
+         name="top-five-youtuber-list"),
 
 ]
