7058809aa0d84404413493f5a6e1046eaf37e6d4
myammedia <admin@myam.media>
2021-03-06 14:02:58 +0530

    added channel subscat url

--- tiys/top_five/urls.py
+++ tiys/top_five/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="top-five-index"),
     path('channel/', views.channel_category_view, name="top-five-channel-category"),
+    path('channel/<slug:slug>/', views.channel_subscat_view, name="top-five-channel-subscat"),
     path('youtuber/', views.youtuber_category_view, name="top-five-youtuber-category"),
     path('youtuber/<slug:slug>/', views.youtuber_subscat_view, name="top-five-youtuber-subscat"),
     path('youtuber/<slug:channel_category>/<slug:subscriber_category>/', views.top_five_youtuber_view,
