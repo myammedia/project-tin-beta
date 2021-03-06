6411ebe6bcf875a333b07ba79b0f66f20ed13a1b
myammedia <admin@myam.media>
2021-03-06 12:27:06 +0530

    added youtuber category url

--- tiys/top_five/urls.py
+++ tiys/top_five/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="top-five-index"),
+    path('youtuber/', views.youtuber_category_view, name="top-five-youtuber-category"),
 
 ]
