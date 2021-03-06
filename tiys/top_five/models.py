61e128b62a40edcc42523472253c0808e33f53cd
myammedia <admin@myam.media>
2021-03-06 12:38:25 +0530

    top five model and migrations

--- tiys/top_five/models.py
+++ tiys/top_five/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from category.models import Channel, Subscriber
 
-# Create your models here.
+
+class Channels(models.Model):
+    CHOICES = (
+        ('Most Subscribers', 'Most Subscribers'),
+        ('Most Videos', 'Most Videos'),
+        ('Most Views', 'Most Views'),
+    )
+
+    channel_category = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    subscriber_category = models.ForeignKey(Subscriber, on_delete=models.CASCADE)
+    type = models.CharField(max_length=50, choices=CHOICES)
+    date = models.DateField()
+    first_yt_name = models.CharField(max_length=150)
+    first_yt_slug = models.SlugField(max_length=100, unique=True)
+    first_yt_data = models.CharField(max_length=150)
+    second_yt_name = models.CharField(max_length=150)
+    second_yt_slug = models.SlugField(max_length=100, unique=True)
+    second_yt_data = models.CharField(max_length=150)
+    third_yt_name = models.CharField(max_length=150)
+    third_yt_slug = models.SlugField(max_length=100, unique=True)
+    third_yt_data = models.CharField(max_length=150)
+    fourth_yt_name = models.CharField(max_length=150)
+    fourth_yt_slug = models.SlugField(max_length=100, unique=True)
+    fourth_yt_data = models.CharField(max_length=150)
+    fifth_yt_name = models.CharField(max_length=150)
+    fifth_yt_slug = models.SlugField(max_length=100, unique=True)
+    fifth_yt_data = models.CharField(max_length=150)
+
+
+class Youtubers(models.Model):
+    CHOICES = (
+        ('Most Subscribers', 'Most Subscribers'),
+        ('Most Videos', 'Most Videos'),
+        ('Most Views', 'Most Views'),
+    )
+
+    channel_category = models.ForeignKey(Channel, on_delete=models.CASCADE)
+    subscriber_category = models.ForeignKey(Subscriber, on_delete=models.CASCADE)
+    type = models.CharField(max_length=50, choices=CHOICES)
+    date = models.DateField()
+    first_yt_name = models.CharField(max_length=150)
+    first_yt_slug = models.SlugField(max_length=100, unique=True)
+    first_yt_data = models.CharField(max_length=150)
+    second_yt_name = models.CharField(max_length=150)
+    second_yt_slug = models.SlugField(max_length=100, unique=True)
+    second_yt_data = models.CharField(max_length=150)
+    third_yt_name = models.CharField(max_length=150)
+    third_yt_slug = models.SlugField(max_length=100, unique=True)
+    third_yt_data = models.CharField(max_length=150)
+    fourth_yt_name = models.CharField(max_length=150)
+    fourth_yt_slug = models.SlugField(max_length=100, unique=True)
+    fourth_yt_data = models.CharField(max_length=150)
+    fifth_yt_name = models.CharField(max_length=150)
+    fifth_yt_slug = models.SlugField(max_length=100, unique=True)
+    fifth_yt_data = models.CharField(max_length=150)
