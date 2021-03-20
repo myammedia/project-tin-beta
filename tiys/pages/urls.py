12998b74d325b1cdf7d37f4ed50ff4fe55ff2fc8
myammedia <admin@myam.media>
2021-03-20 13:35:11 +0530

    Update urls.py

--- tiys/pages/urls.py
+++ tiys/pages/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.index, name="homepage"),
     path('about-us', views.about, name="about-page"),
-    path('submit-detail', views.submit, name="submit-page"),
+    path('submit-details', views.submit, name="submit-page"),
     path('contact-us', views.contact, name="contact-page"),
     path('privacy-policy', views.privacy, name="privacy-page"),
     path('terms-and-conditions', views.terms, name="terms-page"),
