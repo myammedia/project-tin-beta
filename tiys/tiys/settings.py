9062b664f0d341abc9ed4b6264557d4d1d39a31f
myammedia <admin@myam.media>
2021-03-20 12:32:05 +0530

    Update settings.py

--- tiys/tiys/settings.py
+++ tiys/tiys/settings.py
@@ -6,9 +6,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'n#vj7@y7mctvmqbc$@uu#vf2q=-p8!ege9#7zu&gbr(68f@fz4'
 
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['.tiys.in', '65.1.16.143']
+ALLOWED_HOSTS = []
 
 
 ADMINS = [('Admin', 'admin@tiys.in')]
@@ -64,7 +64,7 @@ WSGI_APPLICATION = 'tiys.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
