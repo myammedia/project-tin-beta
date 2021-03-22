863c825ae78afbbe9311ad9c1b676c7b12246444
myammedia <admin@myam.media>
2021-03-22 13:31:25 +0530

    Update admin.py

--- tiys/pages/admin.py
+++ tiys/pages/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . models import Faq
 
-# Register your models here.
+
+class FaqAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+
+admin.site.register(Faq, FaqAdmin)
