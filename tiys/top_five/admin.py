bb1c4a70a661efc9aeedb9c4fd538cd38f2d50df
myammedia <admin@myam.media>
2021-03-22 13:31:30 +0530

    Update admin.py

--- tiys/top_five/admin.py
+++ tiys/top_five/admin.py
@@ -3,7 +3,7 @@ from . models import Channels, Youtubers
 
 
 class ChannelAdmin(admin.ModelAdmin):
-    date_hierarchy = 'date'
+    date_hierarchy = 'pub_date'
     list_display = ('channel_category', 'subscriber_category',)
     list_filter = ('channel_category', 'subscriber_category', 'type',)
     prepopulated_fields = {'first_yt_slug': ('first_yt_name',),
@@ -18,9 +18,9 @@ admin.site.register(Channels, ChannelAdmin)
 
 
 class YoutuberAdmin(admin.ModelAdmin):
-    date_hierarchy = 'date'
+    date_hierarchy = 'pub_date'
     list_display = ('channel_category', 'subscriber_category',)
-    list_filter = ('channel_category', 'subscriber_category', 'date',)
+    list_filter = ('channel_category', 'subscriber_category', 'pub_date',)
 
 
 admin.site.register(Youtubers, YoutuberAdmin)
