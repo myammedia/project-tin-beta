dae314c6152be442456118f103013d0032021d84
myammedia <admin@myam.media>
2021-03-22 13:31:36 +0530

    Create sitemaps.py

---
+++ tiys/channel/sitemaps.py
@@ -0,0 +1,13 @@
+from django.contrib.sitemaps import Sitemap
+from channel.models import ChannelProfile
+
+
+class ChannelProfileSitemap(Sitemap):
+    changefreq = "weekly"
+    priority = 0.5
+
+    def items(self):
+        return ChannelProfile.objects.all()
+
+    def lastmod(self, obj):
+        return obj.pub_date
