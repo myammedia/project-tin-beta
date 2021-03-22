ebe144c8cb1c5461b308afd7ac2c8d1df1838747
myammedia <admin@myam.media>
2021-03-22 13:31:40 +0530

    Create sitemaps.py

---
+++ tiys/pages/sitemaps.py
@@ -0,0 +1,13 @@
+from django.contrib import sitemaps
+from django.urls import reverse
+
+
+class PagesSitemap(sitemaps.Sitemap):
+    priority = 0.5
+    changefreq = 'weekly'
+
+    def items(self):
+        return ['homepage', 'about-page', 'contact-page', 'submit-page', 'terms-page', 'privacy-page', 'faq-page']
+
+    def location(self, item):
+        return reverse(item)
