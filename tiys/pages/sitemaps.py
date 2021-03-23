9d141add4b698f1ec1ed6ff75b6343330ed9ee6a
myammedia <admin@myam.media>
2021-03-23 21:11:59 +0530

    Update sitemaps.py

--- tiys/pages/sitemaps.py
+++ tiys/pages/sitemaps.py
@@ -7,7 +7,9 @@ class PagesSitemap(sitemaps.Sitemap):
     changefreq = 'weekly'
 
     def items(self):
-        return ['homepage', 'about-page', 'contact-page', 'submit-page', 'terms-page', 'privacy-page', 'faq-page']
+        return ['homepage', 'about-page', 'contact-page', 'submit-page', 'terms-page', 'privacy-page', 'faq-page',
+                'channel-index', 'youtuber-index', 'top-five-index', 'top-five-channel-category',
+                'top-five-youtuber-category']
 
     def location(self, item):
         return reverse(item)
