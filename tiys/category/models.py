fda1b98077d88ed85d847d1a8c723bd2a631e01f
myammedia <admin@myam.media>
2021-04-01 13:05:06 +0530

    Update models.py

--- tiys/category/models.py
+++ tiys/category/models.py
@@ -1,15 +1,11 @@
 from django.db import models
-from django.core.files.storage import FileSystemStorage
-
-
-fs = FileSystemStorage(location='/media/images/category')
 
 
 class Channel(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
-    image_png = models.ImageField(blank=True, storage=fs)
-    image_svg = models.FileField(blank=True, storage=fs)
+    image_png = models.ImageField(blank=True, upload_to='images/category')
+    image_svg = models.FileField(blank=True, upload_to='images/category')
 
     def __str__(self):
         return self.name
