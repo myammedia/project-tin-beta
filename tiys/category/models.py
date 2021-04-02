0820ba77a2f2dfd0272c060fb731146304c0c43e
myammedia <admin@myam.media>
2021-04-02 14:00:11 +0530

    Update models.py

--- tiys/category/models.py
+++ tiys/category/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from tiys.tiys.storage_backends import MediaStorage
 
 
 class Channel(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(max_length=100, unique=True)
-    image_png = models.ImageField(blank=True, upload_to='images/category')
-    image_svg = models.FileField(blank=True, upload_to='images/category')
+    image_png = models.ImageField(blank=True, storage=MediaStorage())
+    image_svg = models.FileField(blank=True, storage=MediaStorage())
 
     def __str__(self):
         return self.name
